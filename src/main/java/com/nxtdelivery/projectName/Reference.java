8a4b0a7538c7fa48596dc608bfe6f7b190c28015
nxtdaydelivery <79922345+nxtdaydelivery@users.noreply.github.com>
2021-11-29 18:13:22 +0000

    Add files via upload

---
+++ src/main/java/com/nxtdelivery/projectName/Reference.java
@@ -0,0 +1,11 @@
+package com.nxtdelivery.projectName;
+
+import net.minecraft.util.EnumChatFormatting;
+
+public class Reference {
+    public static final String MODID = "@ID@";
+    public static final String NAME = "@NAME@";
+    public static final String VERSION = "@VER@";
+    public static final String URL = "https://github.com/nxtdaydelivery/@NAME@";
+    public static final EnumChatFormatting COLOR = EnumChatFormatting.GRAY;
+}
